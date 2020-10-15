20efae5da29cf14ce6ec4916a9378daf3158daeb
dcumelles <didac.cumelles.7e3@itb.cat>
2020-10-15 22:02:37 +0200

    function to roll + individual rolling

--- app/src/main/java/cat/itb/diceroller/MainActivity.java
+++ app/src/main/java/cat/itb/diceroller/MainActivity.java
@@ -2,16 +2,22 @@ package cat.itb.diceroller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * @author Dídac Cumelles Cenzano
+ */
+
 public class MainActivity extends AppCompatActivity {
-    TextView resultTextView;
-    Button rollButton;
-    int num;
+    private TextView dau1, dau2;
+    private Button rollButton;
+    private boolean rollable = false;
 
 
     @Override
@@ -19,21 +25,64 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        resultTextView = findViewById(R.id.result_textview);
+        dau1 = findViewById(R.id.dau1);
+        dau2 = findViewById(R.id.dau2);
         rollButton = findViewById(R.id.roll_button);
 
-        //resultTextView.setText("Ekisde");
 
-
-        //Instanciacio anonima de classe
         rollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Botó apretat", Toast.LENGTH_SHORT).show();
-                rollButton.setText("Dice Rolled");
-                num = (int)(Math.random() * (7 - 1) + 1);
-                resultTextView.setText(String.valueOf(num));
+                rollButton.setText(R.string.dices_rolled);
+                rollable=true;
+
+                //No m'agrada com queda
+                //rollButton.setBackgroundColor(Color.parseColor("lime"));
+                roll(dau1);
+                roll(dau2);
+
             }
         });
+
+        dau1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (rollable) {
+                    roll(dau1);
+                }
+            }
+        });
+        dau2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (rollable) {
+                    roll(dau2);
+                }
+            }
+        });
+    }
+
+    public void roll (TextView dau){
+        int num = (int) (Math.random() * (7 - 1) + 1);
+        switch (num) {
+            case 1:
+                dau.setBackgroundResource(R.drawable.dice_1);
+                break;
+            case 2:
+                dau.setBackgroundResource(R.drawable.dice_2);
+                break;
+            case 3:
+                dau.setBackgroundResource(R.drawable.dice_3);
+                break;
+            case 4:
+                dau.setBackgroundResource(R.drawable.dice_4);
+                break;
+            case 5:
+                dau.setBackgroundResource(R.drawable.dice_5);
+                break;
+            case 6:
+                dau.setBackgroundResource(R.drawable.dice_6);
+                break;
+        }
     }
 }
